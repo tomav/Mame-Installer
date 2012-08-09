d7c908101630a0521ec6c32cb1a3748480c47c28
Thomas VIAL <github@ifusio.com>
2012-08-09 10:34:44 +0200

    Added first installer version (still lot of work to be done)

---
+++ mame_installer.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+
+if [ $# -ne 1 ]
+then
+	echo "Error: Missing argument."
+  echo "Usage: ./mame_installer.sh [folder_name]"
+  exit 1
+fi
+
+if [ ! -d "$1" ]; then
+	echo "> Creating folder $1"
+	mkdir $1
+else
+  echo "Error: "$1" folder already exists"
+  # exit 1
+fi
+
+# Source files
+URL_ENDINGS="http://rbelmont.mameworld.info/endings.zip"
+URL_MAME_SOURCE="http://mamedev.mameworld.info/releases/mame0146s.zip"
+URL_MAME_PATCHS=("http://mamedev.org/updates/0146u1_diff.zip" "http://mamedev.org/updates/0146u2_diff.zip" "http://mamedev.org/updates/0146u3_diff.zip" "http://mamedev.org/updates/0146u4_diff.zip")
+
+# Downloading
+cd $1
+USER_AGENT="Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)"
+echo "> Downloading Endings"
+# curl -O $URL_ENDINGS -A "$USER_AGENT" -s
+echo "> Downloading MAME Source"
+# curl -O $URL_MAME_SOURCE "-A $USER_AGENT" -s 
+index=1
+for i in "${URL_MAME_PATCHS[@]}"
+do
+  :
+	echo "> Downloading MAME Patchs #$index"
+	# curl -O $i -A "$USER_AGENT" -s
+	((index++))
+done
+
+# Unziping
+echo "> Unziping files"
+# unzip '*.zip'
+# unzip	mame.zip
+
+# Patching
+echo "> Patching files"
+# ./endings
+# patch -p0 < 0146u1.diff
+# patch -p0 < 0146u2.diff
+# patch -p0 < 0146u3.diff
+# patch -p0 < 0146u4.diff
+
+# Building
+echo "> Let's build ! (have a break, it takes long long time...)"
+make > mame64_build.log
+
